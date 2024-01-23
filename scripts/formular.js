c28779ce2b78c3049408f572bc56cac19f05515d
Shykaro <9372410+Shykaro@users.noreply.github.com>
2024-01-23 23:56:40 +0100

    download and narrator adjustments

--- scripts/formular.js
+++ scripts/formular.js
@@ -36,4 +36,55 @@ document.addEventListener('DOMContentLoaded', () => {
 
     // Initialer Aufruf, um den Zustand des Submit-Buttons zu setzen
     checkFormInputs();
+      //////////////////////////////// NARRATOR CODE ////////////////////////////////
+  const characterIcon = document.getElementById('character-icon');
+  const speechBubble = document.getElementById('speech-bubble');
+
+  // Funktion, um die Speechbubble anzuzeigen
+  function showBubble(tempText, duration) {
+    speechBubble.textContent = tempText;
+    speechBubble.style.animation = ''; // Setzt vorherige Animation zurück
+    speechBubble.classList.add('visible');
+    speechBubble.style.animation = 'blop-in 0.5s ease'; // Startet die Einblend-Animation
+
+    setTimeout(() => {
+      speechBubble.style.animation = 'blop-out 0.5s ease'; // Startet die Ausblend-Animation
+      setTimeout(() => {
+        speechBubble.classList.remove('visible');
+        speechBubble.style.animation = ''; // Setzt Animation zurück
+      }, 500); // Warten, bis die Ausblend-Animation abgeschlossen ist
+    }, duration);
+  }
+
+  // Funktion, um den Text zu verschiedenen Zeiten zu aktualisieren und die Speechbubble zu zeigen
+  function updateBubbleText() {
+    setTimeout(() => {
+      showBubble("Hey, vielen Dank, dass du Max helfen möchtest! Hier hast du zur Belohnung einen Cookie.", 7000);
+    }, 2000);
+
+    setTimeout(() => {
+      showBubble("Es würde uns freuen, wenn du deinen Belohnungs-Cookie annimmst!", 5000);
+    }, 20000);
+
+    setTimeout(() => {
+      showBubble("Bitte nimm den Cookie an.", 5000);
+    }, 35000);
+  }
+
+  updateBubbleText();
+
+  characterIcon.addEventListener('click', () => {
+    if (!speechBubble.classList.contains('visible')) {
+      speechBubble.style.animation = '';
+      speechBubble.classList.add('visible');
+      speechBubble.style.animation = 'blop-in 0.5s ease';
+    } else {
+      speechBubble.style.animation = 'blop-out 0.5s ease';
+      setTimeout(() => {
+        speechBubble.classList.remove('visible');
+        speechBubble.style.animation = '';
+      }, 500);
+    }
+  });
+  //////////////////////////////// NARRATOR END ////////////////////////////////
 });
