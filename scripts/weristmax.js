71c41bcfbf621f9d3cd925de1b638bd0c53efa19
felixChB <lix.brunn@gmail.com>
2024-02-06 01:49:00 +0100

    wer ist max animations

--- scripts/weristmax.js
+++ scripts/weristmax.js
@@ -5,8 +5,22 @@ document.addEventListener('DOMContentLoaded', function () {
     const container3 = document.getElementById('container3');
     const container4 = document.getElementById('container4');
     const container5 = document.getElementById('container5');
+    const werHeader = document.getElementById('wer-header-id');
+
+    const scrollStep1 = 30;
+    const scrollStep2 = 80;
+    const scrollStep3 = 130;
+    const scrollStep4 = 180;
+    const scrollStep5 = 230;
+    const scrollStepLast = 280;
+
     let isAnimating = false;
     var scrollStep = 0;
+
+    window.scrollTo(0, 0);
+
+    window.addEventListener("scroll", setScrollVar);
+    window.addEventListener("resize", setScrollVar);
 
     // Listen for wheel events to detect scrolling
     document.addEventListener('wheel', function (event) {
@@ -43,21 +57,144 @@ document.addEventListener('DOMContentLoaded', function () {
 
             // Move the second container based on the scroll direction
             container2.style.transform = direction === 'up' ? 'translateX(100%)' : 'translateX(0)';
-        
-            if ( scrollStep != 0) {
+
+            if (scrollStep != 0) {
                 scrollStep--;
             }
             console.log('scrollStep: ' + scrollStep)
         }
 
         isAnimating = false;
-
-        // Simulate loading or any other asynchronous task
-        /* setTimeout(function () {
-            // Reset the containers' positions after the animation
-            container.style.transform = 'translateX(0)';
-            container2.style.transform = 'translateX(100%)';
-            isAnimating = false;
-        }, 500); */
     }
+
+    function moveContainer() {
+
+    }
+
+    function setScrollVar() {
+        const htmlElement = document.documentElement;
+        const percentOfScreenHeightScrolled = (htmlElement.scrollTop / htmlElement.clientHeight) * 100;
+        console.log(percentOfScreenHeightScrolled)
+        htmlElement.style.setProperty("--scroll", percentOfScreenHeightScrolled);
+
+        //header animations
+        if (percentOfScreenHeightScrolled <= scrollStep1) {
+            werHeader.classList.remove('wer-moved');
+        }
+        if (percentOfScreenHeightScrolled > scrollStep1 && percentOfScreenHeightScrolled < scrollStep2) {
+            werHeader.classList.add('wer-moved');
+            werHeader.classList.remove('wer-out');
+        }
+        if (percentOfScreenHeightScrolled >= scrollStep2) {
+            werHeader.classList.remove('wer-moved');
+            werHeader.classList.add('wer-out');
+        }
+
+        //container1 animations
+        if (percentOfScreenHeightScrolled <= scrollStep1) {
+            container1.classList.remove('container1-moved');
+        }
+        if (percentOfScreenHeightScrolled > scrollStep1 && percentOfScreenHeightScrolled < scrollStep2) {
+            container1.classList.add('container1-moved');
+            container1.classList.remove('container1-out');
+        }
+        if (percentOfScreenHeightScrolled >= scrollStep2) {
+            container1.classList.remove('container1-moved');
+            container1.classList.add('container1-out');
+        }
+        //container2 animations
+        if (percentOfScreenHeightScrolled <= scrollStep2) {
+            container2.classList.remove('container2-moved');
+        }
+        if (percentOfScreenHeightScrolled > scrollStep2 && percentOfScreenHeightScrolled < scrollStep3) {
+            container2.classList.add('container2-moved');
+            container2.classList.remove('container2-out');
+        }
+        if (percentOfScreenHeightScrolled >= scrollStep3) {
+            container2.classList.remove('container2-moved');
+            container2.classList.add('container2-out');
+        }
+        //container3 animations
+        if (percentOfScreenHeightScrolled <= scrollStep3) {
+            container3.classList.remove('container3-moved');
+        }
+        if (percentOfScreenHeightScrolled > scrollStep3 && percentOfScreenHeightScrolled < scrollStep4) {
+            container3.classList.add('container3-moved');
+            container3.classList.remove('container3-out');
+        }
+        if (percentOfScreenHeightScrolled >= scrollStep4) {
+            container3.classList.remove('container3-moved');
+            container3.classList.add('container3-out');
+        }
+        //container4 animations
+        if (percentOfScreenHeightScrolled <= scrollStep4) {
+            container4.classList.remove('container4-moved');
+        }
+        if (percentOfScreenHeightScrolled > scrollStep4 && percentOfScreenHeightScrolled < scrollStep5) {
+            container4.classList.add('container4-moved');
+            container4.classList.remove('container4-out');
+        }
+        if (percentOfScreenHeightScrolled >= scrollStep5) {
+            container4.classList.remove('container4-moved');
+            container4.classList.add('container4-out');
+        }
+        //container5 animations
+        if (percentOfScreenHeightScrolled <= scrollStep5) {
+            container5.classList.remove('container5-moved');
+        }
+        if (percentOfScreenHeightScrolled > scrollStep5 && percentOfScreenHeightScrolled < scrollStepLast) {
+            container5.classList.add('container5-moved');
+            container5.classList.remove('container5-out');
+        }
+        /* if (percentOfScreenHeightScrolled >= 400) {
+            container5.classList.remove('container5-moved');
+            container5.classList.add('container5-out');
+        } */
+    }
+
+    setScrollVar();
+
+    const zoom = document.querySelector('.zoom');
+    const minZoom = 1;
+    const maxZoom = 2;
+
+    /* addEventListener('scroll', e => {
+        /* const vh = window.innerHeight / 100;
+        const scrollTop = document.documentElement.scrollTop;
+        const start = 50 * vh;
+        const stop = 150 * vh;
+        if (scrollTop > start && scrollTop < stop) {
+            //const scale = Math.max(2.2 - (scrollTop - start) / 500, 1);
+            //zoom.style.transform = `scale(${scale})`;
+            const distance = (scrollTop - start) / 10;
+            container1.style.left = `${((100 - distance) * -1)}%`;
+            var opacity = Math.max(100 - (scrollTop - start) / 500, 100);
+            container1.style.opacity = `${opacity}%`;
+        }
+
+        if (scrollTop > (20 * vh) && scrollTop < (80 * vh)) {
+            var h1Scale = Math.max(1.7 - (scrollTop - start) / 500, 1);
+            var h1Top = (scrollTop - start) / 10;
+            werHeader.style.transform = `scale(${h1Scale})`;
+            werHeader.style.top = `${10 + h1Top}%`;
+        }
+
+        const vh = window.innerHeight / 100;
+        const scrollTop = document.documentElement.scrollTop;
+        const totalHeight = document.documentElement.scrollHeight - window.innerHeight;
+        const startMoving = 0.2 * totalHeight;
+        const stopMoving = 0.8 * totalHeight;
+
+        // Move the element along with the scroll when 20% to 80% of the page is scrolled
+        if (scrollTop >= startMoving && scrollTop <= stopMoving) {
+            const distance = (scrollTop - startMoving) / (stopMoving - startMoving);
+            const topPosition = -30 + (distance * 20);
+            werHeader.style.top = `${topPosition}%`;
+        }
+
+        // After 80% of the page is scrolled, fix the element at -10%
+        if (scrollTop > stopMoving) {
+            werHeader.style.top = '-10%';
+        }
+    }) */
 });
